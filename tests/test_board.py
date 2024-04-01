f70d7f906863e42f938c81644c74cb155ba96877
Kenneth <kencubed@gmail.com>
2024-04-01 00:19:37 -0400

    added fixtures

--- tests/test_board.py
+++ tests/test_board.py
@@ -1,11 +1,27 @@
 """Tests for the Board class."""
 
 import pytest
+from santorini.worker import Worker
 from santorini.game import Board
 
 @pytest.fixture
-def sample_board():
-    '''A list sample card names.'''
+def worker_a1():
+    """A sample worker for player a."""
+    return Worker(player = 'a', worker_id = '1')
+
+@pytest.fixture
+def worker_a2():
+    """A second sample worker for player a."""
+    return Worker(player = 'a', worker_id = '2')
+
+@pytest.fixture
+def worker_b1():
+    """A sample worker for player b."""
+    return Worker(player = 'b', worker_id = '3')
+
+@pytest.fixture
+def board():
+    '''A sample board.'''
     return Board()
 
 def test(sample_board):
