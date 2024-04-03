e954d79a40882144a4301d34f790be3446bc7262
Kenneth Allen <kencubed@gmail.com>
2024-04-02 23:37:24 -0400

    renamed player attribute to player_id. added god_card attribute

--- santorini/player.py
+++ santorini/player.py
@@ -1,8 +1,9 @@
 class Player:
     """Player class to manage player actions and workers."""
 
-    def __init__(self, player = None):
-        self.player = player
+    def __init__(self, player_id = "", god_card = None):
+        self.player_id = player_id
+        self._god_card = god_card
 
     def __bool__(self):
-        return bool(self.player)
+        return bool(self.player_id)
